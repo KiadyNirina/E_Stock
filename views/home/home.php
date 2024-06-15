f6fbe1411ed541645349ddd2bb7ed1fe1154e570
Kiady Nirina <kiady142ram@gmail.com>
2024-06-14 16:23:00 -1200

    Add table in html

--- views/home/home.php
+++ views/home/home.php
@@ -15,6 +15,12 @@
         <hr>
         
         <div class="">
+            <ul>
+                <li><b>Nombres des équipements total : </b>200</li>
+                <li><b>Prix total : </b>2.500.000 ariary</li>
+                <li><b>Total des équipements entrées : </b>50</li>
+                <li><b>Total des équipements sorties : </b>20</li>
+            </ul>
             <div class="add">
                 <a href="">
                     <img src="../../static/img/icons/ajouter.png" alt="">
@@ -22,5 +28,36 @@
                 </a>
             </div>
         </div>
+
+        <hr>
+
+        <table>
+            <thead>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">N° devis</th>
+                    <th scope="col">Nom</th>
+                    <th scope="col">Prix unitaire</th>
+                    <th scope="col">Nombres</th>
+                    <th scope="col">Date du commande</th>
+                    <th scope="col">Date de livraison</th>
+                    <th scope="col">Etat</th>
+                    <th scope="col">action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>SD21091136</td>
+                    <td>speaker</td>
+                    <td>560 000 Ariary</td>
+                    <td>6</td>
+                    <td>25-12-2024</td>
+                    <td>31-12-2024</td>
+                    <td>Entrée</td>
+                    <td><a href="a"><img src="/static/img/icons/modifier.png" alt=""></a> <a href="b"><img src="/static/img/icons/supprimer.png" alt=""></a></td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
